bd4e61a8f6f92290e7cc6f078893b047407afe65
Ben Roos <benhroos@icloud.com>
2015-04-16 16:22:33 -0700

    Added part1 folder and code

---
+++ part1/State.java
@@ -0,0 +1,50 @@
+package final_project_part_1;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class State {
+    private HashSet<Integer> givenCard;
+    private HashMap<Integer, Double> exposedToCard;
+    private int depth;
+    private double reward;
+    private double heuristicReward;
+    public State parent;
+    
+    public State(HashSet<Integer> givenCard, HashMap<Integer, Double> exposedToCard, double reward, double heuristicReward, int depth) {
+        this.givenCard = givenCard;
+        this.exposedToCard = exposedToCard;
+        this.reward = reward;
+        this.heuristicReward = heuristicReward;
+        this.depth = depth;
+        parent = null;
+    }
+    
+    public void addPerson(int person) {
+        givenCard.add(person);
+    }
+    
+    public double getReward() {
+        return reward;
+    }
+    
+    public double getHeuristicReward() {
+        return heuristicReward;
+    }
+    
+    public boolean isGoal() {
+        return depth == 10;
+    }
+    
+    public HashSet<Integer> getGivenCard() {
+        return givenCard;
+    }
+    
+    public HashMap<Integer, Double> getExposedToCard() {
+        return exposedToCard;
+    }
+    
+    public int getDepth() {
+        return depth;
+    }
+}
